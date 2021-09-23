02244ee70c9b08d59ae25ab352ec77a06bc622fd
Polcsi <pollakbence12@gmail.com>
2021-09-23 13:46:56 +0200

    Add ct and css changes

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const moon = document.querySelector('.moon');
 const clouds = document.querySelector('.cloud');
 const stars = document.querySelector('.stars');
 
-daybtn.onclick = () => {
+function setDay()
+{
     document.body.style.backgroundImage = "linear-gradient(var(--day-sky)";
     moon.style.display = "none";
     sun.style.display = "unset"
@@ -15,7 +16,8 @@ daybtn.onclick = () => {
     daybtn.style.display = "none";
 }
 
-nightbtn.onclick = () => {
+function setNight()
+{
     document.body.style.backgroundImage = "linear-gradient(var(--night-sky)";
     moon.style.display = "unset";
     sun.style.display = "none"
@@ -23,4 +25,42 @@ nightbtn.onclick = () => {
     stars.style.display = "unset";
     nightbtn.style.display = "none";
     daybtn.style.display = "flex";
+}
+
+daybtn.onclick = () => {
+    setDay();
+}
+
+nightbtn.onclick = () => {
+    setNight();
+}
+
+function checkTime()
+{
+    const time = new Date().toLocaleTimeString();
+    console.log(time);
+    if(time > "07:00:00" && time < "19:00:00")
+    {
+        console.log("Good Day!");
+        setDay();
+    } else if(time > "19:00:00" && time < "07:00:00") {
+        console.log("Good night!");
+        setNight();
+    }
+}
+
+checkTime();
+
+function display_c()
+{
+    var refresh=1000; 
+    mytime=setTimeout('display_ct()',refresh)
+}
+    
+function display_ct() 
+{
+    var x = new Date().toLocaleTimeString();
+    /* var ampm = x.substring(0, 2) >= 12 ? ' PM' : ' AM'; */
+    document.getElementById('ct').innerHTML = x;
+    display_c();
 }
